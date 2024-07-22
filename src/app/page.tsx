318fd77cf092159b728ee26414276500a3e44f11
donghyun1998 <donghyun1998@naver.com>
2024-07-22 15:51:14 +0900

    feat: rendering관련 주석 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,10 @@ const SsrFalse = dynamic(() => import("@/app/_component/ssr-false"), {
 });
 
 export default function Home() {
-  // dynamic rendering : DB조회같이 동적인 값을 서버에서 조회하고 싶을 때 사용
-  // static rendering : 빌드할때 실행되고 다시 그 페이지를 렌더링 하지 않을 때 사용
+  // dynamic rendering : DB조회같이 동적인 값을 서버에서 조회하고 싶을 때 사용합니다
+  // 클라이언트가 연결 요청 시 서버에서 만들어서 내려줍니다
+  // static rendering(기본값) : 빌드할때 실행되고 다시 그 페이지를 렌더링 하지 않을 때 사용
+  // 빌드 시 서버에서 만들어 정적 html로 만들어 두고 클라이언트가 요청할 때 내려줍니다
 
   return (
     <>
